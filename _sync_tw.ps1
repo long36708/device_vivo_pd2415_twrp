e43471dddcc76365ef83caeb04cebd893966cf5c
longmo <3131521901@qq.com>
2026-09-06 17:17:46 +0800

    chore: untrack local analysis scratch scripts; ignore them

--- _sync_tw.ps1
+++
@@ -1,6 +0,0 @@
-$base='f:/learn-front/learn-hook/vivo-twrp/device_vivo_pd2415_twrp'
-$bc=Join-Path $base 'BoardConfig.mk'
-$t=[System.IO.File]::ReadAllText($bc)
-$t=$t -replace "`r`n","`n"
-[System.IO.File]::WriteAllText($bc,$t)
-Write-Host "BoardConfig.mk normalized to LF"
